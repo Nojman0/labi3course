febcff473340eafd3985d6ff4b730b9a29d4a002
dudka egorka <fassston@gmail.com>
2019-06-09 14:41:32 +0300

    testirovanie

---
+++ Testirovanie/SumServiceTests/SumServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testirovanie;
+using NUnit.Framework;
+
+namespace Testirovanie
+{
+    [TestFixture]
+    public class SumServiceюTests
+    {
+        [Test]
+        // arrange (организация)
+        public void Sum_One_And_One()
+        {
+            var sumService = new SumService();
+            // act (акт)
+            var result = sumService.Sum(1, 1);
+            // assert (утверждение)
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ Testirovanie/Testirovanie/SumService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testirovanie
+{
+    public class SumService
+    {
+        public int Sum(int arg1, int arg2)
+        {
+            var result = arg1 + arg2;
+            AllOperationsSum += result;
+            return result;
+        }
+        public int AllOperationsSum { get; set; }
+    }
+
+}
